97c1074738417d438e851678ca094e18add8eb4b
engrbugs <engrbugs@gmail.com>
2020-09-19 23:58:47 -0700

    2020-09-19-2358

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const tooltip = document.getElementById("tooltip");
 const colorLength = 9;
 
 async function run() {
-  console.log('run');
   const eduResp = await fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json');
   const educations = await eduResp.json();
   
@@ -12,6 +11,8 @@ async function run() {
   var width = $(window).width(),
     height = $(window).height(),
     xyrPadding = 40;
+
+  console.log(height);
 
   const path = d3.geoPath();
   const data = topojson.feature(counties, counties.objects.counties).features;
@@ -54,14 +55,11 @@ async function run() {
     .attr('data-education', d => educations.find(edu => edu.fips === d.id).bachelorsOrHigher)
     .attr('d', path)
     .on("mousemove", (d, item) => {
-      console.log(d, item,item.id);
       tooltip.style.left = d.pageX + (xyrPadding / 2) + "px";
       tooltip.style.top = d.pageY - xyrPadding + "px";
       const education = educations.find(edu => edu.fips === item.id);
-      console.log(education.bachelorsOrHigher);
       tooltip.innerHTML = 
       `${education.area_name}, ${education.state} ${education.bachelorsOrHigher}%`;
-      console.log(education);
       tooltip.setAttribute("data-education", education.bachelorsOrHigher);
     })
     .on("mouseover", () => (tooltip.style.visibility = "visible"))
